f3ba5e950c6f165e898906b043186b2734bd6aff
Andreas Monitzer <andreas@monitzer.com>
2020-08-30 01:30:00 +0200

    Dead code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![recursion_limit = "512"]
 
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 use yew::prelude::*;
 
 mod app;
@@ -20,23 +19,7 @@ pub fn main_js() -> Result<(), JsValue> {
 
     App::<app::Main>::new().mount_to_body();
 
-    // let scanned_closure = Closure::wrap(Box::new(scanned) as Box<dyn FnMut(String)>);
-    // let error_closure = Closure::wrap(
-    //     Box::new(move |err: JsValue| console::log_1(&err)) as Box<dyn FnMut(JsValue)>
-    // );
-
-    // SCANNER.with(|scanner| {
-    //     scanner.render(&scanned_closure, &error_closure);
-    // });
-
-    // scanned_closure.forget();
-    // error_closure.forget();
-
     Ok(())
-}
-
-fn scanned(text: String) {
-    console::log_2(&JsValue::from_str("SCANNED:"), &JsValue::from_str(&text));
 }
 
 fn subtle() -> web_sys::SubtleCrypto {
